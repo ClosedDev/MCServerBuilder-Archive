9efc46d2e8837e0a75009f18437e29b56e399d2c
Render <79672833+Ren-deRing@users.noreply.github.com>
2023-04-21 21:43:42 +0900

    ⚡️➕ 일부 코드 최적화 / 언어 매니저 추가

---
+++ src/main/java/io/github/closeddev/LangManager.java
@@ -0,0 +1,28 @@
+package io.github.closeddev;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LangManager {
+    private static JSONObject langData;
+
+    static {
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader reader = new FileReader(Main.MCSBPath + "/lang.json");
+            langData = (JSONObject) parser.parse(reader);
+            reader.close();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getText(String key, String lang) {
+        JSONObject used = (JSONObject) langData.get(lang);
+        return used.get(key).toString();
+    }
+}
