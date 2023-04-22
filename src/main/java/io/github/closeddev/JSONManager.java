8d04306b673fd974d9b0dcb7651fb6c264b3690b
Render <79672833+Ren-deRing@users.noreply.github.com>
2023-04-22 23:02:30 +0900

    자동 빌더 테스트

--- src/main/java/io/github/closeddev/JSONManager.java
+++ src/main/java/io/github/closeddev/JSONManager.java
@@ -5,18 +5,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
-
-public class JSONManager {
-    public static JSONObject loadJSON(String filepath) {
-        JSONParser parser = new JSONParser();
-        JSONObject jsonObject;
-        try (Reader reader = new FileReader(filepath)) {
-             jsonObject = (JSONObject) parser.parse(reader);
-        } catch (IOException | ParseException e) {
-            throw new RuntimeException(e);
-        }
-        return jsonObject;
-    }
+ㅁㄴㅇㄻㅊㅍㅁㄴㅇㅍㅊ
 
     public static void writeJSON(String filepath, JSONObject jsonObject) throws IOException {
         FileWriter savefile = new FileWriter(filepath);
